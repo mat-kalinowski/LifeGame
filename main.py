97c7d9d2accdaf11ce4519b227bb838196af9370
Mateusz Kalinowski <mateusz.kalinowski.97@gmail.com>
2019-11-16 14:51:39 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import numpy as np
+from copy import deepcopy
+
+
+def nowa_plansza(H, W, pola=None):
+    if H > 0 and W > 0 and H % 1 == 0 and W % 1 == 0:
+        plansza = np.zeros((H, W), dtype=float)
+
+        if pola:
+            for pair in pola:
+                if pair[0] < H and pair[1] < W:
+                    plansza[pair[0], pair[1]] = 1
+                else:
+                    print('blad')
+                    return None
+            return plansza
+        else:
+            print('blad')
+            return None
+
+
+def sasiedzi(plansza, i, j):
+    point = plansza[i, j]
+    plansza = plansza[max(0, i-1):i+2, max(0, j-1): j+2]
+    s_num = np.count_nonzero(plansza, None)
+
+    if point > 0:
+        s_num = max(0, s_num - 1)
+
+    return s_num
+
+
+def krok(plansza):
+    obecna_plansza = deepcopy(plansza)
+
+    for i in range(len(plansza)):
+        for j in range(len(plansza[i])):
+            neigh_num = sasiedzi(plansza, i, j)
+
+            if neigh_num == 3 and plansza[i, j] == 0:
+                obecna_plansza[i, j] = 1
+            elif neigh_num < 2 and plansza[i, j] > 0:
+                obecna_plansza[i, j] = 0
+            elif neigh_num > 3 and plansza[i, j] > 0:
+                obecna_plansza[i, j] = 0
+            elif plansza[i, j] > 0:
+                obecna_plansza[i, j] /= 2
+
+    return obecna_plansza
+
+
+def symulacja(plansza, n=100):
+    lista = [deepcopy(plansza)]
+    for i in range(n):
+        lista.append(krok(lista[-1]))
+
+    return lista
+
+
+arr = [(0, 0), (1, 1), (0, 1)]
+game_arr = nowa_plansza(6, 6, arr)
+sym_arr = symulacja(game_arr, 2)
+
+print(sym_arr.__str__())
